28aad3e5104103ae078ab4c962fc9a54923c1d79
lichen911 <rhatch@gmail.com>
2024-01-11 21:36:00 -0500

    Add support for other worlds

--- src/main/java/io/github/lichen911/waypoints/WpCommand.java
+++ src/main/java/io/github/lichen911/waypoints/WpCommand.java
@@ -3,7 +3,9 @@ package io.github.lichen911.waypoints;
 
 import java.util.Map;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -42,18 +44,20 @@ public class WpCommand implements CommandExecutor {
 	private Location getLocationFromConfig(Player player, String wpName, String wpType) {
 		String playerName = player.getPlayerListName();
 		String configPath = this.getConfigPath(playerName, wpName, wpType);
-		String coords = this.plugin.getConfig().getString(configPath);
+		String coord = this.plugin.getConfig().getString(configPath);
 		
 		Location location = null;
-		if (coords != null) {
-			String[] coordArray = coords.split("\\s+");
+		if (coord != null) {
+			String[] coordArray = coord.split("\\s+");
 			
 			double x = (double) Integer.parseInt(coordArray[0]);
 			double y = (double) Integer.parseInt(coordArray[1]);
 			double z = (double) Integer.parseInt(coordArray[2]);
+			
+			String worldName = coordArray[3];
+			World world = Bukkit.getWorld(worldName);
 	
-			// TODO: Review to see if we should ensure that World is always the Overworld?
-			location = new Location(player.getWorld(), x, y, z);
+			location = new Location(world, x, y, z);
 		}
 		return location;
 	}
@@ -61,11 +65,12 @@ public class WpCommand implements CommandExecutor {
 	private void addWaypoint(Player player, String wpName, String wpType) {
 		Location location = player.getLocation();
 		String playerName = player.getPlayerListName();
-    	String coords = location.getBlockX() + " " + location.getBlockY() + " " + location.getBlockZ();
+    	String coord = location.getBlockX() + " " + location.getBlockY() + " " + location.getBlockZ();
     	String configPath = this.getConfigPath(playerName, wpName, wpType);
+    	String worldName = player.getWorld().getName();
     	
-    	player.sendMessage("Creating waypoint named '" + wpName + "' at: " + coords);
-    	this.plugin.getConfig().set(configPath, coords);
+    	player.sendMessage("Creating waypoint named '" + wpName + "' at " + coord);
+    	this.plugin.getConfig().set(configPath, coord + " " + worldName);
     	this.plugin.saveConfig();
 		
 	}
@@ -80,6 +85,26 @@ public class WpCommand implements CommandExecutor {
 		
 	}
 	
+	private void printWaypointConfigMap(ConfigurationSection waypointMapConfigSection, Player player, String wpTypeDesc) {
+		if (waypointMapConfigSection != null) {
+			Map<String, Object> publicWaypointMap = waypointMapConfigSection.getValues(false);
+			
+			player.sendMessage(wpTypeDesc + " waypoints:");
+			for (Map.Entry<String, Object> entry : publicWaypointMap.entrySet()) {
+				String coordStr = (String) entry.getValue();
+				String[] coordArray = coordStr.split("\\s+");
+				String x = coordArray[0];
+				String y = coordArray[1];
+				String z = coordArray[2];
+				String world = coordArray[3];
+				
+			    player.sendMessage("  " + entry.getKey() + " -> " + x + ", " + y + ", " + z + " [" + world + "]");
+			}
+		} else {
+			player.sendMessage("No " + wpTypeDesc + " waypoints");
+		}
+	}
+	
 	private void listWaypoint(Player player) {
 		String playerName = player.getPlayerListName();
 		String configPlayerPath = configPlayersPrefix + "." + playerName;
@@ -89,28 +114,9 @@ public class WpCommand implements CommandExecutor {
 				.getConfigurationSection(configPlayerPath);
 				
 
-		if (publicWaypointMapSection != null) {
-			Map<String, Object> publicWaypointMap = publicWaypointMapSection.getValues(false);
-			
-			player.sendMessage("Public waypoints:");
-			for (Map.Entry<String, Object> entry : publicWaypointMap.entrySet()) {
-			    player.sendMessage("  " + entry.getKey() + " - " + entry.getValue());
-			}
-		} else {
-			player.sendMessage("No public waypoints");
-		}
+		this.printWaypointConfigMap(publicWaypointMapSection, player, "public");
+		this.printWaypointConfigMap(privateWaypointMapSection, player, "private");
 		
-		if (privateWaypointMapSection != null) {
-			Map<String, Object> privateWaypointMap = privateWaypointMapSection.getValues(false);
-
-			player.sendMessage();
-			player.sendMessage("Private waypoints:");
-			for (Map.Entry<String, Object> entry : privateWaypointMap.entrySet()) {
-			    player.sendMessage("  " + entry.getKey() + " - " + entry.getValue());
-			}
-		} else {
-			player.sendMessage("No private waypoints");
-		}
 	}
 	
 	private void setWaypoint(Player player, String wpName, String wpType) {
@@ -126,6 +132,10 @@ public class WpCommand implements CommandExecutor {
 	
 	
 	private void tpWaypoint(Player player, String wpName, String wpType) {
+	    // Whenever a player teleports first store their current location in
+	    // a waypoint named "last".
+	    this.addWaypoint(player, "last", wpType);
+	    
 		Location location = this.getLocationFromConfig(player, wpName, wpType);
 		if (location != null) {
 			player.sendMessage("Teleporting to waypoint '" + wpName + "'");
