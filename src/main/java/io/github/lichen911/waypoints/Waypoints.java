ad954b448fbfa55bc3ae3655aaf2c83226a5edb8
lichen911 <rhatch@gmail.com>
2024-01-12 15:45:34 -0500

    Add bStats support

--- src/main/java/io/github/lichen911/waypoints/Waypoints.java
+++ src/main/java/io/github/lichen911/waypoints/Waypoints.java
@@ -1,10 +1,13 @@
 package io.github.lichen911.waypoints;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Waypoints extends JavaPlugin {
     @Override
     public void onEnable() {
-         getCommand(CommandLiteral.WP).setExecutor(new WpCommand(this));
+        Metrics metrics = new Metrics(this, 20727);
+
+        getCommand(CommandLiteral.WP).setExecutor(new WpCommand(this));
     }
 }
